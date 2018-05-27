51cf3e97a43bf8ebf4713860c3b6c50acc228701
Oleg Lipskiy <lolego1601@gmail.com>
2018-05-27 10:47:06 +0300

    feat: add multidex and di support in application class

--- app/src/main/java/com/rtfmarket/RtfmarketApplication.kt
+++ app/src/main/java/com/rtfmarket/RtfmarketApplication.kt
@@ -1,6 +1,10 @@
+package com.rtfmarket
 
 import android.app.Application
+import android.content.Context
+import android.support.multidex.MultiDex
 import com.crashlytics.android.Crashlytics
+import com.rtfmarket.di.ComponentsManager
 import io.fabric.sdk.android.Fabric
 
 
@@ -9,6 +13,12 @@ class RtfmarketApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        ComponentsManager.initAppComponent(this)
         Fabric.with(this, Crashlytics())
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }

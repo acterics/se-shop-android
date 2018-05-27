9067b5ed307086db5fd59bba71f7fefa7783c962
Oleg Lipskiy <lolego1601@gmail.com>
2018-05-27 09:44:47 +0300

    build: add fabric support

---
+++ app/src/main/java/com/rtfmarket/RtfmarketApplication.kt
@@ -0,0 +1,14 @@
+
+import android.app.Application
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+
+
+class RtfmarketApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Fabric.with(this, Crashlytics())
+    }
+}

71fafb3c5ebafb03e3180a9c965b7b5bb3990500
Oleg Lipskiy <lolego1601@gmail.com>
2018-05-28 17:46:27 +0300

    ci: fabric integration done

--- scripts/generate-release-notes.sh
+++ scripts/generate-release-notes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESULT_PATH=./release_notes.txt
-
+RESULT_PATH=$TRAVIS_BUILD_DIR/app/release_notes.txt
+COUNT=$(echo $1 | tr -s "." "\n" | wc -l)
 echo "Start generating release notes"
-git log -n $1 --pretty=format:%s > $RESULT_PATH
+git log -n $1 --pretty=format:%s $COUNT> $RESULT_PATH

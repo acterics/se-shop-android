2069e52cde26977120139bca2ce80b5c79dfcdec
Oleg Lipskiy <lolego1601@gmail.com>
2018-05-30 01:47:18 +0300

    feat: add stub product fragment

---
+++ app/src/main/java/com/rtfmarket/ui/bottomnavigation/product/ProductFragment.kt
@@ -0,0 +1,20 @@
+package com.rtfmarket.ui.bottomnavigation.product
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rtfmarket.R
+import com.rtfmarket.common.BaseFragment
+
+class ProductFragment: BaseFragment() {
+
+    override fun injectComponent() {
+
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_product, container, false)
+    }
+}

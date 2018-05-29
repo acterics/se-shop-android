2caf3e5a62782fafa7017405602af720cd873c42
Oleg Lipskiy <lolego1601@gmail.com>
2018-05-29 13:41:02 +0300

    ci: disable release notes logging

--- scripts/generate-release-notes.sh
+++ scripts/generate-release-notes.sh
@@ -2,4 +2,4 @@
 RESULT_PATH=$2/app/release_notes.txt
 COUNT=$(echo $1 | tr -s "." "\n" | wc -l)
 echo "Start generating release notes"
-git log -n $COUNT --pretty=format:%s > $RESULT_PATH
+git log -n ${COUNT} --pretty=format:%s > ${RESULT_PATH}
